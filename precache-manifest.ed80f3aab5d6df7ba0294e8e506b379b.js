ae4cf6359f226aa67c56f3d635ff090db7496027
Azhar-Kamel <azhar.kamel@gmail.com>
2020-07-12 12:01:14 +0200

    Updates

---
+++ precache-manifest.ed80f3aab5d6df7ba0294e8e506b379b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7cf1c8852c922b8dcd0098fd42b09e",
+    "url": "/React-Js-Interview-Task/index.html"
+  },
+  {
+    "revision": "9050563e49cfe614b65f",
+    "url": "/React-Js-Interview-Task/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cf31d2519d63d813b803",
+    "url": "/React-Js-Interview-Task/static/css/main.7c73b0b4.chunk.css"
+  },
+  {
+    "revision": "9050563e49cfe614b65f",
+    "url": "/React-Js-Interview-Task/static/js/2.ede44464.chunk.js"
+  },
+  {
+    "revision": "6338e51f1d5b9df223e8dea62a7afe8d",
+    "url": "/React-Js-Interview-Task/static/js/2.ede44464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf31d2519d63d813b803",
+    "url": "/React-Js-Interview-Task/static/js/main.639bc5be.chunk.js"
+  },
+  {
+    "revision": "b5befa0c4e0b18c5a302",
+    "url": "/React-Js-Interview-Task/static/js/runtime-main.faa6da95.js"
+  }
+]);
